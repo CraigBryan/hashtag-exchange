2bd204007378b637c0445749fdc6b3e1daa73734
Craig Bryan <craig.bryan@360pi.com>
2017-06-07 22:31:31 -0400

    Added ensure-indices docker script
    Issue: add_ensure_indices_script

---
+++ scripts/ensure_indices/ensure_indices.js
@@ -0,0 +1,73 @@
+var yaml = require('js-yaml');
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var CONFIG_PATH = '/scripts/ensure-indices/config.yaml';
+var indexErrors = [];
+
+function getDb(config_path) {
+  let mongoHost = 'localhost';
+  let mongoPort = null;
+  let mongoUser = null;
+  let mongoPass = null;
+  let dbName = null;
+
+  try {
+    let config = yaml.safeLoad(fs.readFileSync(config_path, 'utf8'));  
+    mongoPort = config['vms']['DIGITAL_OCEAN_MONGO_PORT'];
+    mongoUser = config['users']['MONGO_DB_TWITTER_USER'];
+    mongoPass = config['passwords']['MONGO_DB_TWITTER_PASSWORD'];
+    dbName = config['db-constants']['HASHTAG_EXCHANGE_DB'];
+  } catch (err) {
+    console.log("Error reading configuration");
+    console.log(err);
+    process.exit(1);
+  }
+
+  let mongoUrl = `mongodb://${}:${}@${mongoHost}:${mongoPort}/${dbName}`;
+  let connection = null;
+
+  MongoClient.connect(mongoUrl, (err, conn) => {
+    assert.equal(err, null);
+    connection = conn;
+  });
+
+  console.log(`Successfully connected to ${mongoUrl}`);
+  return connection;
+};
+
+// Ensures there are indices on the rawTwitterHashtags collection 
+function ensureRawTwitterData(db) {
+  console.log('Indexing the "rawTwitterHashtags" collection');
+
+  console.log('Ensuring index on the "createdOn" field');
+  db.ensureIndex(
+    'rawTwitterHashtags',
+    {'createdOn': 1},
+    {'background': true},
+    function(err, indexName) {
+      if (err != null) {
+        indexErrors.push(err);
+      } else {
+        console.log('"createdOn" index exists');
+      }
+    };
+  );
+
+  console.log('Finished indexing the "rawTwitterHashtags" collection');
+};
+
+// Add more collection indexing functions here
+
+var db = getDb(CONFIG_PATH);
+ensureRawTwitterData(db);
+// call more collection indexing functions here
+
+if (indexErrors.length() != 0) {
+  console.log('Errors occurred during ensureIndex');
+  console.log(indexErrors);
+  process.exit(1);
+};
+
+console.log('Successfully ensured indices');
