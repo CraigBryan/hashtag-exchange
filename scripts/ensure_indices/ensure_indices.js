e9ccd644b23f98974c498fd6cb0a6cbe358434a8
Craig Bryan <craig.bryan@360pi.com>
2017-06-11 17:24:00 -0400

    Removed unix-specific paths from dockerfile
    Added ttl of 31 days to raw data index, added some environment variables
    
    Issue: add_ensure_indices_script

--- scripts/ensure_indices/ensure_indices.js
+++ scripts/ensure_indices/ensure_indices.js
@@ -1,21 +1,34 @@
+/*
+This script can use the following optional environment variables:
+CONFIG_FILE_PATH - the path for the config file
+                   default: /scripts/ensure-indices/config.yaml
+MONGO_HOST - the host address of the mongo server
+             default: localhost
+MONGO_PORT - the port of the mongo server
+             default: from config file
+*/
+
 var yaml = require('js-yaml');
 var fs = require('fs');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
-var CONFIG_PATH = '/scripts/ensure-indices/config.yaml';
+var CONFIG_PATH = process.env.CONFIG_FILE_PATH ||
+                  '/scripts/ensure-indices/config.yaml';
 var indexErrors = [];
 
 function getDb(config_path) {
-  let mongoHost = 'localhost';
+  let mongoHost = null;
   let mongoPort = null;
   let mongoUser = null;
   let mongoPass = null;
   let dbName = null;
 
   try {
-    let config = yaml.safeLoad(fs.readFileSync(config_path, 'utf8'));  
-    mongoPort = config['vms']['DIGITAL_OCEAN_MONGO_PORT'];
+    let config = yaml.safeLoad(fs.readFileSync(config_path, 'utf8'));
+    mongoHost = process.env.MONGO_HOST || 'localhost';
+    mongoPort = process.env.MONGO_PORT || 
+                config['vms']['DIGITAL_OCEAN_MONGO_PORT'];
     mongoUser = config['users']['MONGO_DB_TWITTER_USER'];
     mongoPass = config['passwords']['MONGO_DB_TWITTER_PASSWORD'];
     dbName = config['db-constants']['HASHTAG_EXCHANGE_DB'];
@@ -46,6 +59,7 @@ function ensureRawTwitterData(db) {
     'rawTwitterHashtags',
     {'createdOn': 1},
     {'background': true},
+    {'expireAfterSeconds': 31 * 24 * 60 * 60}, // 31 days
     function(err, indexName) {
       if (err != null) {
         indexErrors.push(err);
